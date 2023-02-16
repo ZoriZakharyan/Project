c313544426347703b8e5c1833374ca2e4d0f646e
Зори Захарян <Zori33@mail.ru>
2023-02-16 16:39:16 +0300

    localStorage"

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -5,7 +5,10 @@ const apiRouter = express.Router();
 
 apiRouter.patch('/count/:id', async (req, res) => {
   try {
+    // const { id } = req.params;
+    // const like = id;
     await TemsBody.update({ count: sequelize.literal('count +1') }, { where: { id: req.params.id } });
+    //   req.session[like] = id;
     return res.sendStatus(200);
   } catch (erro) {
     console.log(erro);
@@ -14,8 +17,11 @@ apiRouter.patch('/count/:id', async (req, res) => {
 });
 apiRouter.patch('/-count/:id', async (req, res) => {
   try {
-    await TemsBody.update({ count: sequelize.literal('count -1') }, { where: { id: req.params.id } });
-    return res.sendStatus(200);
+    if (req.session.like) {
+      await TemsBody.update({ count: sequelize.literal('count -1') }, { where: { id: req.params.id } });
+      res.session.destroy();
+      return res.sendStatus(200);
+    }
   } catch (erro) {
     console.log(erro);
     return res.sendStatus(500);
